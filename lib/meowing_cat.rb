0f15b7fb82822e358255391e2313506a4cf7e33a
Patrick Harrison <jahman@bellatlantic.net>
2019-08-07 00:59:40 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -4,8 +4,9 @@ class Cat
 #maru.name= "Maru"
   end## code your solution here. 
    def name=(cats_name)
-      @
-  this_cats_name = cats_nameend
+      
+  maru.name = "Maru"
+  end
 def meow
   puts"meow!"
 end
